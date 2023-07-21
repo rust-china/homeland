1d79ca29fad2c4068adeb11d1c45dcf7a8e692d6
sanmu <578595193@qq.com>
2023-07-21 17:12:16 +0800

    TEST: 0.0.1

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
-declare interface Window {
-  __SSR_STATE__: Record<string, any>;
+declare global {
+  interface Window {
+    __SSR_STATE__: Record<string, any>;
+  }
 }
 
 import type { App as VueApp, VNode } from 'vue'

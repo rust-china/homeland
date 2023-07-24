29ef57feb4aada765a81cb4668b4685a0eb89505
sanmu <578595193@qq.com>
2023-07-24 21:57:43 +0800

    FEAT: add markdown help

--- frontend/src/components/shared/MarkdownVditor.vue
+++ frontend/src/components/shared/MarkdownVditor.vue
@@ -1,6 +1,8 @@
 <script lang="tsx" setup>
 import Vditor from 'vditor'
 import { usePreferredDark } from '@vueuse/core'
+import { useDialog } from '@/utils/hooks/useDialog'
+import MarkdownHelp from './MarkdownHelp.vue'
 import 'vditor/dist/index.css'
 import 'vditor/dist/css/content-theme/light.css'
 import 'vditor/dist/css/content-theme/dark.css'
@@ -13,6 +15,8 @@ const props = defineProps({
 	options: [Object],
 })
 const emits = defineEmits(['update:modelValue'])
+
+const dialog = useDialog()
 
 let vditor: Vditor | null = null
 const vditorRef = ref()
@@ -37,6 +41,27 @@ onMounted(() => {
 			enable: true
 		},
 		toolbar: [
+			{
+				name: '排版',
+				tip: '排版说明',
+				className: 'left',
+				icon: '<svg t="1690201489775" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3200" width="200" height="200"><path d="M512 272c-35.3 0-64-28.7-64-64s28.7-64 64-64 64 28.7 64 64-28.7 64-64 64z m0 64c70.7 0 128-57.3 128-128S582.7 80 512 80s-128 57.3-128 128 57.3 128 128 128zM576 480v384c0 8.8-7.2 16-16 16h-96c-8.8 0-16-7.2-16-16V480c0-8.8 7.2-16 16-16h96c8.8 0 16 7.2 16 16z m-192-16v416c0 35.3 28.7 64 64 64h128c35.3 0 64-28.7 64-64V464c0-35.3-28.7-64-64-64H448c-35.3 0-64 28.7-64 64z" p-id="3201"></path></svg>',
+				click() {
+					dialog.create({
+						header: '排版说明',
+						width: '80%',
+						cancelBtn: null,
+						confirmBtn: null,
+						slots: {
+							default: () => (
+								<div>
+									<MarkdownHelp></MarkdownHelp>
+								</div>
+							)
+						}
+					})
+				}
+			},
 			"emoji",
 			"headings",
 			"bold",
